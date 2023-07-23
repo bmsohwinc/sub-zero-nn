5761fafb8265ec0ea8a4b84885aceadc8bea2bc2
Sourabha <bharasou8@gmail.com>
2023-07-23 23:12:03 +0530

    cleanup

--- tf-js/2d-tfjs/new/index.js
+++
@@ -1,6 +0,0 @@
-async function loadData() {
-    let loader = document.getElementById('div-overlay');
-    loader.style.visibility = 'visible';
-    const data = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
-    loader.style.visibility = 'hidden';
-}
